07abde7bf393a528a3b3fe2cda457c5a4cb0df42
GRUPOPLENUM\msoberanis <majesova@gmail.com>
2017-09-15 11:18:11 -0500

    Demo de log

---
+++ WebApplicationMvc/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using AppFramework.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplicationMvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        ILogger logger = new LoggerFileImplementation();
+        public HomeController()
+        {
+            //inicializar en el constructor
+            logger.EnsureInitialized();
+        }
+
+        public ActionResult Index()
+        {
+            //Un info
+            logger.Info(this, "Este es un info de Index iniciado");
+            //tiene sobrecargas
+            logger.Info(this.GetType(), "Esta es otra info");
+            logger.Info(this.GetType(), "Otra info con exception", new Exception("Ah nu ma"));
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+            try
+            {
+                int zero = 0;
+                int i = 1 / zero;
+            }
+            catch (Exception ex) {
+                //Un error no controlado
+                logger.Error(this, ex);
+            }
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            //Se esaa misma orma chambean Warn y Fatal.
+            logger.Warn(this, "WARNING");
+            return View();
+        }
+    }
+}
---
+++ WebApplicationMvc/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general sobre un ensamblado se controla mediante lo siguiente
+// conjunto de atributos. Cambie los valores de estos atributos para modificar la información
+// asociada con un ensamblado.
+[assembly: AssemblyTitle("WebApplicationMvc")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WebApplicationMvc")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si configura ComVisible como falso, los tipos de este ensamblado no se hacen visibles
+// para componentes COM.  Si necesita acceder a un tipo de este ensamblado desde
+// COM, establezca el atributo ComVisible en True en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como ID de typelib si este proyecto se expone a COM
+[assembly: Guid("aed423eb-6240-464b-a7eb-2488e08e8499")]
+
+// La información de versión de un ensamblado consta de los siguientes cuatro valores:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+// Puede especificar todos los valores o puede predeterminar los números de compilación y de revisión
+// mediante el carácter '*', como se muestra a continuación:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
